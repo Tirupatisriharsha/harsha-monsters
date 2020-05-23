b6f30fc22f0f0a7e2d22ca4ffe9903ff35d793d1
Sriharsha Tirupati <Sriharsha.Tirupati@gisbizdev.gisbiz.chennai.com>
2020-05-23 13:27:25 +0530

    Updates

---
+++ precache-manifest.15b71ea7ab77adae2cd2f1211a1f60a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d49384ffbc32aa30ce13b67957ee898",
+    "url": "/harsha-monsters/index.html"
+  },
+  {
+    "revision": "3e8d27652d500227cb2d",
+    "url": "/harsha-monsters/static/css/main.289d7ce3.chunk.css"
+  },
+  {
+    "revision": "0101600ff48593f641e8",
+    "url": "/harsha-monsters/static/js/2.129ef368.chunk.js"
+  },
+  {
+    "revision": "3e8d27652d500227cb2d",
+    "url": "/harsha-monsters/static/js/main.a4b7e104.chunk.js"
+  },
+  {
+    "revision": "6d5d2c6d201c91c214ff",
+    "url": "/harsha-monsters/static/js/runtime-main.eb1bf67f.js"
+  }
+]);
